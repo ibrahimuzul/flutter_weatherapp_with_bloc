6e510030c0f9a64afa628b6e0135a88e0c94ad84
iuzul <iuzul@IBRAHIMUZUL>
2020-11-19 16:52:50 +0300

    211. Havanın Durumuna Göre Themedatanın Değiştirilmesi Bölüm 10

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-
 import 'blocs/weather/bloc.dart';
+import 'blocs/weather/tema/bloc.dart';
 import 'locator.dart';
-
 import 'widget/weather_app.dart';
 
 void main() {
   setupLocator();
-  runApp(MyApp());
+  runApp(BlocProvider<TemaBloc>(
+    builder: (context) => TemaBloc(),
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Weather App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocBuilder(
+      bloc: BlocProvider.of<TemaBloc>(context),
+      builder: (context, TemaState state) => MaterialApp(
+        title: 'Weather App',
+        debugShowCheckedModeBanner: false,
+        theme: (state as UygulamaTemasi).tema,
+        home: BlocProvider<WeatherBloc>(
+            builder: (context) => WeatherBloc(), child: WeatherApp()),
       ),
-      home: BlocProvider<WeatherBloc>(
-          builder: (context) => WeatherBloc(), child: WeatherApp()),
     );
   }
 }
--- lib/widget/weather_app.dart
+++ lib/widget/weather_app.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_weatherapp_with_bloc/blocs/weather/bloc.dart';
+import 'package:flutter_weatherapp_with_bloc/blocs/weather/tema/bloc.dart';
 
 import 'hava_durumu_resim.dart';
 import 'location.dart';
@@ -27,7 +28,6 @@ class WeatherApp extends StatelessWidget {
               onPressed: () async {
                 kullanicininSectigiSehir = await Navigator.push(context,
                     MaterialPageRoute(builder: (context) => SehirSecWidget()));
-                debugPrint("Seçilen sehir: " + kullanicininSectigiSehir);
                 if (kullanicininSectigiSehir != null) {
                   _weatherBloc.dispatch(
                       FetchWeatherEvent(sehirAdi: kullanicininSectigiSehir));
@@ -38,6 +38,7 @@ class WeatherApp extends StatelessWidget {
       body: Center(
         child: BlocBuilder(
           bloc: _weatherBloc,
+          // ignore: missing_return
           builder: (context, WeatherState state) {
             if (state is InitialWeatherState) {
               return Center(
@@ -51,6 +52,14 @@ class WeatherApp extends StatelessWidget {
             }
             if (state is WeatherLoadedState) {
               final getirilenWeather = state.weather;
+              final _havaDurumKisaltma =
+                  getirilenWeather.consolidatedWeather[0].weatherStateAbbr;
+              /* final _temaBloc = BlocProvider.of<TemaBloc>(context);
+              _temaBloc.dispatch(event)*/
+
+              BlocProvider.of<TemaBloc>(context).dispatch(
+                  TemaDegistirEvent(havaDurumuKisaltmasi: _havaDurumKisaltma));
+
               _refreshCompleter.complete();
               _refreshCompleter = Completer();
 
